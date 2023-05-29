886aa0e528e99abe4d0665d73f515eb1a9cdd28b
Michał <michkola1999@gmail.com>
2023-05-29 23:40:51 +0200

    v1

--- app/src/main/java/com/example/po_proj03/ui/calendar/PlanFragment.kt
+++ app/src/main/java/com/example/po_proj03/ui/calendar/PlanFragment.kt
@@ -22,32 +22,31 @@ import com.example.po_proj03.ui.notifications.NotificationsViewModel
 import org.json.JSONArray
 
 
-public class PlanFragment : Fragment() {
+class PlanFragment : Fragment() {
 
     private var _binding: FragmentPlanBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
     private val viewModel: NotificationsViewModel by activityViewModels()
 
-    var data =  JSONArray();
-     var dCList : MutableList<DanceClasses>? = null
-    final val colorsMap:Map<String,Int?> = mapOf("Trening indywidualny par turniejowych" to Color.parseColor("blue"),
-        "Grupa Turniejowa" to Color.parseColor("#636100"), "Grupa Dorosłych Zaawansowana" to Color.parseColor("#634061"),
-        "Grupa Lady Solo" to Color.parseColor("#636120"), "Grupa Dorosłych Początkująca" to Color.parseColor("#635061"),
-        "Grupa przedszkolna" to Color.parseColor("#636140"), "Grupa Zaawansowana Dziecięca" to Color.parseColor("#637061"),
-        "Żeliwsławki Grupa Dorosłych" to Color.parseColor("#636611"))
+    var data = JSONArray();
+    var dCList: MutableList<DanceClasses>? = null
+    val colorsMap: Map<String, Int?> = mapOf(
+        "Trening indywidualny par turniejowych" to Color.parseColor("#949263"),
+        "Grupa Turniejowa" to Color.parseColor("#FFFEEB"),
+        "Grupa Dorosłych Zaawansowana" to Color.parseColor("#E0DFB8"),
+        "Grupa Lady Solo" to Color.parseColor("#CAB8E0"),
+        "Grupa Dorosłych Początkująca" to Color.parseColor("#F4EBFF"),
+        "Grupa przedszkolna" to Color.parseColor("#796394"),
+        "Grupa Zaawansowana Dziecięca" to Color.parseColor("#DFD8A0"),
+        "Żeliwsławki Grupa Dorosłych" to Color.parseColor("#DFDAD6")
+    )
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View {
-        val planViewModel =
-                ViewModelProvider(this).get(PlanViewModel::class.java)
-
         _binding = FragmentPlanBinding.inflate(inflater, container, false)
         val root: View = binding.root
         val btn = root.findViewById<Button>(com.example.po_proj03.R.id.button4)
@@ -63,18 +62,16 @@ public class PlanFragment : Fragment() {
 
         viewModel.filters.observe(viewLifecycleOwner, Observer {
             this.data = viewModel.filters.value!!
-            // Perform an action with the latest item data.
         })
-        Log.d("console","PLAN "+data.toString())
+        Log.d("console", "PLAN " + data.toString())
         btn.setOnClickListener {
             this.data = JSONArray()
             btn.setBackgroundColor(Color.YELLOW)
             viewModel.filters.observe(viewLifecycleOwner, Observer {
                 this.data = viewModel.filters.value!!
-                // Perform an action with the latest item data.
             })
-            Log.d("console","btnPLAN "+data.toString())
-            dCList=null
+            Log.d("console", "btnPLAN " + data.toString())
+            dCList = null
             dataToDC()
             clearTable(contentRow)
             if (dCList?.isNotEmpty() == true) {
@@ -130,8 +127,7 @@ public class PlanFragment : Fragment() {
                     }
                 }
 
-            }
-            else{
+            } else {
                 textView.text = "no lessons matching filter"
             }
 
@@ -155,16 +151,17 @@ public class PlanFragment : Fragment() {
             }
         }
     }
-    fun dCToString(dC:DanceClasses):String{
-var str = ""
-str+="zajęcia: "+dC.summary+"\n"
-        str+="lokalizacja: "+dC.location+"\n"
-        str+="start: "+dC.start+"\n"
-        str+="koniec: "+dC.end+"\n"
+
+    fun dCToString(dC: DanceClasses): String {
+        var str = ""
+        str += "zajęcia: " + dC.summary + "\n"
+        str += "lokalizacja: " + dC.location + "\n"
+        str += "start: " + dC.start + "\n"
+        str += "koniec: " + dC.end + "\n"
         return str
     }
 
-fun addText(str:String,linearLayout: LinearLayout,color:Int){
+    fun addText(str: String, linearLayout: LinearLayout, color: Int) {
         val lparams = LinearLayout.LayoutParams(
             LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT
         )
@@ -173,24 +170,16 @@ fun addText(str:String,linearLayout: LinearLayout,color:Int){
         tv.text = str
         linearLayout.addView(tv)
         tv.setBackgroundColor(color)
-}
-  fun clearTable(tableRow: TableRow){
+    }
 
-      val childCount: Int = tableRow.childCount
-      for (i in 0 until childCount) {
-          val ll: LinearLayout = tableRow.getChildAt(i) as LinearLayout
-          ll.removeAllViews()
-          /*
-          val childCountLl: Int = ll.childCount
-          for (j in 0 until childCountLl) {
-              val v:TextView = ll.getChildAt(j) as TextView
-              //Log.i("console", v.text as String)
+    fun clearTable(tableRow: TableRow) {
 
-          }
-
-           */
-      }
-  }
+        val childCount: Int = tableRow.childCount
+        for (i in 0 until childCount) {
+            val ll: LinearLayout = tableRow.getChildAt(i) as LinearLayout
+            ll.removeAllViews()
+        }
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
@@ -200,81 +189,3 @@ fun addText(str:String,linearLayout: LinearLayout,color:Int){
 
 }
 
-
-/*
-class CalendarFragment : Fragment() {
-
-    private var _binding: FragmentCalendarBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
-    val RESULT = "result"
-    final val EVENT = "event"
-    private val ADD_NOTE = 44
-
-    var mCalendarView: CalendarView? = null
-    private val mEventDays: List<EventDay> = ArrayList()
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val calendarViewModel =
-            ViewModelProvider(this).get(CalendarViewModel::class.java)
-
-        _binding = FragmentCalendarBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        mCalendarView = root.findViewById(com.example.po_proj03.R.id.calendarView)
-
-        mCalendarView!!.setOnDayClickListener(OnDayClickListener(){
-            fun onDayClick(eventDay:EventDay) {
-                previewNote(eventDay);
-            }
-        })
-
-        /*
-        val events: MutableList<EventDay> = ArrayList()
-
-        val calendar: Calendar = Calendar.getInstance()
-        events.add(EventDay(calendar, com.example.po_proj03.R.drawable.sample_icon)!!)
-
-        val calendarView: CalendarView = root.findViewById(com.example.po_proj03.R.id.calendarView)
-        calendarView.setEvents(events)
-
-        calendarView.setOnDayClickListener { eventDay ->
-            val clickedDayCalendar = eventDay.calendar
-        }
-         */
-
-        return
-    }
-    /*
-    fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
-        if (requestCode == ADD_NOTE && resultCode == RESULT_OK) {
-            val myEventDay: MyEventDay = data.getParcelableExtra(RESULT)
-            mCalendarView.setDate(myEventDay.getCalendar())
-            mEventDays.add(myEventDay)
-            mCalendarView!!.setEvents(mEventDays)
-        }
-
-    }
-    */
-
-    fun previewNote(eventDay: EventDay) {
-        val intent = Intent(this, NotePreviewActivity::class.java)
-        if (eventDay is MyEventDay) {
-            intent.putExtra(EVENT, eventDay as MyEventDay)
-        }
-        startActivity(intent)
-    }
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-
-}
-*/
